ef13740fd76373d32f344a2b2456fd828fa6f9cd
KittenLord <vanya.braslavets@gmail.com>
2025-01-19 20:14:21 +0200

    building

--- Main.hs
+++ Main.hs
@@ -1,6 +1,112 @@
 module Main where
 import MyRandom
 
+unreservedChars = [ 'a'..'z' ] ++ [ 'A'..'Z' ] ++ [ '0'..'9' ] ++ [ '-', '.', '_', '~' ]
+
+reservedChars = genericDelimsChars ++ subDelimsChars
+genericDelimsChars = ":/?#[]@"
+subDelimsChars = "!$&'()*+,;="
+
+schemeChars = [ 'a'..'z' ] ++ [ 'A'..'Z'] ++ [ '0'..'9' ] ++ [ '+', '-', '.' ]
+schemeInitChars = [ 'a'..'z' ] ++ [ 'A'..'Z' ]
+
+hexChars = [ '0'..'9' ] ++ [ 'a'..'f' ] ++ [ 'A'..'F' ]
+
+emptyList :: Int -> [()]
+emptyList x
+    | x <= 0 = []
+    | otherwise = () : emptyList (x - 1)
+
+genValidScheme :: Random String
+genValidScheme = do
+    init <- randomElem schemeInitChars
+    len <- randomRange (0, 10)
+    list <- sequence $ map (\_ -> randomElem schemeChars) (emptyList len)
+    return $ init : list
+
+genValidColon :: String
+genValidColon = [ ':' ]
+
+data HierType = HierAuthority | HierAbsolute | HierRootless | HierEmpty
+    deriving (Show, Eq, Bounded, Enum)
+
+genValidUserInfo :: Random String
+genValidUserInfo = undefined
+
+genValidPort :: Random String
+genValidPort = undefined
+
+genValidHost :: Random String
+genValidHost = undefined
+
+genValidAuthority :: Random String
+genValidAuthority = do
+    useUserInfo <- randomPercent 15
+    usePort <- randomPercent 30
+    concat <$> (sequence [ if useUserInfo then (++ "@") <$> genValidUserInfo else pure "", genValidHost, if usePort then (":" ++) <$> genValidPort else pure "" ])
+
+
+genValidPercentEncoding :: Random String
+genValidPercentEncoding = do
+    x <- randomElem hexChars
+    y <- randomElem hexChars
+    return [ '%', x, y ]
+
+genValidPchar :: Random String
+genValidPchar = do
+    percent <- randomPercent 30
+    if percent then
+        genValidPercentEncoding
+    else
+        (\x -> [x]) <$> randomElem (unreservedChars ++ subDelimsChars ++ "@:")
+
+genValidPathSegment :: Random String
+genValidPathSegment = do
+    len <- randomRange(0, 10)
+    concat <$> (sequence $ map (\_ -> genValidPchar) (emptyList len))
+
+genValidAbemptyPath :: Random String
+genValidAbemptyPath = do
+    p <- randomPercent 40
+    if p then
+        pure ""
+    else
+        concat <$> (sequence [ pure "/", genValidPathSegment, genValidAbemptyPath ])
+
+genValidAbsolutePath :: Random String
+genValidAbsolutePath = do
+    rest <- randomPercent 85
+    if rest then do
+        init <- genValidPchar
+        segment <- genValidPathSegment
+        path <- genValidAbemptyPath
+        return $ "/" ++ init ++ segment ++ path
+    else
+        return "/"
+
+genValidRootlessPath :: Random String
+genValidRootlessPath = do
+    init <- genValidPchar
+    segment <- genValidPathSegment
+    path <- genValidAbemptyPath
+    return $ init ++ segment ++ path
+
+
+    
+
+genValidHier :: Random String
+genValidHier = do
+    opt <- randomElem [ HierEmpty, HierAuthority, HierAbsolute, HierRootless ]
+    case opt of
+        HierEmpty -> pure ""
+        HierAuthority -> concat <$> sequence [ pure "//", genValidAuthority, genValidAbemptyPath ]
+        HierAbsolute -> genValidAbsolutePath
+        HierRootless -> genValidRootlessPath
+    
+genValidUri :: Random String
+genValidUri = concat <$> (sequence [ genValidScheme, pure genValidColon, genValidHier ])
+
 main :: IO ()
 main = do
-    print "hello"
+    let uris = map (\s -> seed genValidUri s) [0..20]
+    print uris
--- MyRandom.hs
+++ MyRandom.hs
@@ -20,15 +20,19 @@ random = Random $ \g -> let x = (rngA * g + rngC) `mod` rngM in
 instance Functor Random where
     fmap f (Random t) = Random $ \g -> let (a, g') = t g in (f a, g')
 
+-- NOTE: ideally all of these should be g, not g g' and g'', but for some reason ghosting didn't work as I expected
 instance Applicative Random where
     pure a = Random $ \g -> (a, g)
     (Random fc) <*> (Random ac) = Random $
-        \g -> let (f, g) = fc g in
-              let (a, g) = ac g in
-              (f a, g)
+        \g -> let (f, g') = fc g in
+              let (a, g'') = ac g' in
+              (f a, g'')
 
 instance Monad Random where
-    (Random ac) >>= f = Random $ (\(a, g') -> let (Random bc) = f a in bc g') . ac
+    (Random ac) >>= f = Random $ (\(a, g) -> let (Random bc) = f a in bc g) . ac
+
+randomRange :: (Int, Int) -> Random Int
+randomRange (lo, hi) = (\r -> (r `mod` (hi - lo) + lo)) <$> random
 
 seed :: Random a -> Rng -> a
 seed (Random ac) s = let (a, g) = ac s in a
@@ -37,4 +41,14 @@ randomElem :: [a] -> Random a
 randomElem xs = do
     a <- random
     let i = a `mod` length xs
-    return $ xs !! a
+    return $ xs !! i
+
+-- TODO: for some reason this doesn't work
+randomBound :: (Enum a, Bounded a) => Random a
+randomBound = randomElem [ minBound..maxBound ]
+
+randomPercent :: Int -> Random Bool
+randomPercent x
+    | x >= 100 = pure True
+    | x <= 0 = pure False
+    | otherwise = (< x) <$> randomRange (0, 100)
