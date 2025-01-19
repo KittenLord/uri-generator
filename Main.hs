2a4ae1f87553de1acb665cdb04d6ea3f0c40d1c6
KittenLord <vanya.braslavets@gmail.com>
2025-01-19 21:59:47 +0200

    made everything typed instead of strings

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 import MyRandom
+import Control.Monad
 import Data.List
 
 unreservedChars = [ 'a'..'z' ] ++ [ 'A'..'Z' ] ++ [ '0'..'9' ] ++ [ '-', '.', '_', '~' ]
@@ -13,23 +14,51 @@ schemeInitChars = [ 'a'..'z' ] ++ [ 'A'..'Z' ]
 
 hexChars = [ '0'..'9' ] ++ [ 'a'..'f' ] ++ [ 'A'..'F' ]
 
-emptyList :: Int -> [()]
-emptyList x
+dummyList :: Int -> [()]
+dummyList x
     | x <= 0 = []
-    | otherwise = () : emptyList (x - 1)
+    | otherwise = () : dummyList (x - 1)
+
+orElse :: Maybe a -> a -> a
+orElse Nothing a = a
+orElse (Just a) _ = a
 
 genValidScheme :: Random String
 genValidScheme = do
     init <- randomElem schemeInitChars
     len <- randomRange (0, 10)
-    list <- sequence $ map (\_ -> randomElem schemeChars) (emptyList len)
+    list <- sequence $ map (\_ -> randomElem schemeChars) (dummyList len)
     return $ init : list
 
-genValidColon :: String
-genValidColon = [ ':' ]
+data Host = HostIPLiteral String
+          | HostIPv4 String
+          | HostRegName String
 
-data HierType = HierAuthority | HierAbsolute | HierRootless | HierEmpty
-    deriving (Show, Eq, Bounded, Enum)
+instance Show Host where
+    show (HostIPLiteral v) = v
+    show (HostIPv4 v) = v
+    show (HostRegName v) = v
+
+data Hier = HierAuthority (Maybe String) Host (Maybe String) [String]
+          | HierAbsolute [String]
+          | HierRootless [String]
+          | HierEmpty
+
+showRootlessPath :: [String] -> String
+showRootlessPath = intercalate "/"
+
+showAbemptyPath :: [String] -> String
+showAbemptyPath = join . map ('/' :)
+
+instance Show Hier where
+    show HierEmpty = ""
+    show (HierRootless path) = showRootlessPath path
+    show (HierAbsolute path) = showAbemptyPath path
+    show (HierAuthority userinfo host port path) =
+        let userinfo' = ((++ "@") <$> userinfo) `orElse` "" in
+        let port' = ((':' :) <$> port) `orElse` "" in
+        userinfo' ++ show host ++ port' ++ showAbemptyPath path
+
 
 genValidPcharString :: Random String -> Random String
 genValidPcharString gen = do
@@ -41,38 +70,38 @@ genValidPcharString gen = do
     else
         pure ""
 
-genValidUserInfo :: Random String
-genValidUserInfo = genValidPcharString (genValidPcharRaw ":")
+genValidUserInfo :: Random (Maybe String)
+genValidUserInfo = do
+    empty <- randomPercent 80
+    if empty then pure Nothing
+    else Just <$> genValidPcharString (genValidPcharRaw ":")
 
-genValidPort :: Random String
-genValidPort = genValidPcharString ((\x -> [x]) <$> randomElem [ '0'..'9' ])
+genValidPort :: Random (Maybe String)
+genValidPort = do
+    empty <- randomPercent 50
+    if empty then pure Nothing
+    else Just <$> genValidPcharString ((\x -> [x]) <$> randomElem [ '0'..'9' ])
 
-data HostType = HostIPLiteral | HostIPv4 | HostRegName
+genValidHostRegName :: Random Host
+genValidHostRegName = HostRegName <$> genValidPcharString (genValidPcharRaw "")
 
-genValidHostRegName :: Random String
-genValidHostRegName = genValidPcharString (genValidPcharRaw "")
-
-genValidHostIPv4 :: Random String
-genValidHostIPv4 = do
-    (intercalate ".") <$> (sequence $ map (\_ -> show <$> randomRange (0, 255)) (emptyList 4))
+genValidHostIPv4 :: Random Host
+genValidHostIPv4 = (HostIPv4 . intercalate ".") <$> (sequence $ map (\_ -> show <$> randomRange (0, 255)) (dummyList 4))
 
 -- TODO: implement
 genValidHostIPLiteral :: Random String
 genValidHostIPLiteral = undefined
 
-genValidHost :: Random String
-genValidHost = do
-    opt <- randomElem [ {-HostIPLiteral,-} HostIPv4, HostRegName ]
-    case opt of
-        HostRegName -> genValidHostRegName
-        HostIPv4 -> genValidHostIPv4
-        HostIPLiteral -> genValidHostIPLiteral
+genValidHost :: Random Host
+genValidHost = join $ randomElem [ {-HostIPLiteral,-} genValidHostIPv4, genValidHostRegName ]
 
-genValidAuthority :: Random String
+genValidAuthority :: Random Hier
 genValidAuthority = do
-    useUserInfo <- randomPercent 15
-    usePort <- randomPercent 30
-    concat <$> (sequence [ if useUserInfo then (++ "@") <$> genValidUserInfo else pure "", genValidHost, if usePort then (":" ++) <$> genValidPort else pure "" ])
+    userInfo <- genValidUserInfo
+    host <- genValidHost
+    port <- genValidPort
+    path <- genValidAbemptyPath
+    pure $ HierAuthority userInfo host port path
 
 genValidPercentEncoding :: Random String
 genValidPercentEncoding = do
@@ -91,64 +120,69 @@ genValidPcharRaw extra = do
 genValidPchar :: Random String
 genValidPchar = genValidPcharRaw "@:"
 
-genValidPathSegment :: Random String
-genValidPathSegment = do
-    len <- randomRange(0, 10)
-    concat <$> (sequence $ map (\_ -> genValidPchar) (emptyList len))
+genValidPathSegment :: Int -> Random String
+genValidPathSegment min = do
+    len <- randomRange(min, 10)
+    concat <$> (sequence $ map (\_ -> genValidPchar) (dummyList len))
 
-genValidAbemptyPath :: Random String
+genValidAbemptyPath :: Random [String]
 genValidAbemptyPath = do
-    p <- randomPercent 40
-    if p then
-        pure ""
-    else
-        concat <$> sequence [ pure "/", genValidPathSegment, genValidAbemptyPath ]
+    empty <- randomPercent 40
+    if empty then
+        return []
+    else do
+        segment <- genValidPathSegment 0
+        (++ [segment]) <$> genValidAbemptyPath
 
-genValidAbsolutePath :: Random String
+genValidAbsolutePath :: Random [String]
 genValidAbsolutePath = do
-    rest <- randomPercent 85
-    if rest then do
-        init <- genValidPchar
-        segment <- genValidPathSegment
-        path <- genValidAbemptyPath
-        return $ "/" ++ init ++ segment ++ path
-    else
-        return "/"
+    single <- randomPercent 40
+    if single then
+        pure [""]
+    else do
+        segment <- genValidPathSegment 1
+        (++ [segment]) <$> genValidAbemptyPath
 
-genValidRootlessPath :: Random String
-genValidRootlessPath = do
-    init <- genValidPchar
-    segment <- genValidPathSegment
-    path <- genValidAbemptyPath
-    return $ init ++ segment ++ path
+genValidRootlessPath = genValidAbsolutePath
 
-genValidHier :: Random String
-genValidHier = do
-    opt <- randomElem [ HierEmpty, HierAuthority, HierAbsolute, HierRootless ]
-    case opt of
-        HierEmpty -> pure ""
-        HierAuthority -> concat <$> sequence [ pure "//", genValidAuthority, genValidAbemptyPath ]
-        HierAbsolute -> genValidAbsolutePath
-        HierRootless -> genValidRootlessPath
+genValidHier :: Random Hier
+genValidHier = join $ randomElem [ pure HierEmpty, genValidAuthority, HierAbsolute <$> genValidAbsolutePath, HierRootless <$> genValidRootlessPath ]
 
-genValidQuery :: Random String
+genValidQuery :: Random (Maybe String)
 genValidQuery = do
     empty <- randomPercent 70
-    if empty then pure ""
-    else
-        ('?' :) <$> genValidPcharString (genValidPcharRaw "@:/?")
+    if empty then pure Nothing
+    else Just <$> genValidPcharString (genValidPcharRaw "@:/?")
 
-genValidFragment :: Random String
+genValidFragment :: Random (Maybe String)
 genValidFragment = do
     empty <- randomPercent 70
-    if empty then pure ""
-    else
-        ('#' :) <$> genValidPcharString (genValidPcharRaw "@:/?")
+    if empty then pure Nothing
+    else Just <$> genValidPcharString (genValidPcharRaw "@:/?")
 
-genValidUri :: Random String
-genValidUri = concat <$> sequence [ genValidScheme, pure genValidColon, genValidHier, genValidQuery, genValidFragment ]
+data Uri = Uri String Hier (Maybe String) (Maybe String)
+
+instance Show Uri where
+    show (Uri scheme hier query fragment) =
+        let query' = (('?' :) <$> query) `orElse` "" in
+        let fragment' = (('#' :) <$> fragment) `orElse` "" in
+        scheme ++ ":" ++ show hier ++ query' ++ fragment'
+
+genValidUri :: Random Uri
+genValidUri = do
+    scheme <- genValidScheme
+    hier <- genValidHier
+    query <- genValidQuery
+    fragment <- genValidFragment
+    return $ Uri scheme hier query fragment
 
 main :: IO ()
 main = do
     let uris = map (\s -> seed genValidUri s) [0..20]
-    print uris
+    printUris uris
+
+printUris :: [Uri] -> IO ()
+printUris [] = return ()
+printUris (x:xs) = do
+    putStrLn $ show x
+    printUris xs
