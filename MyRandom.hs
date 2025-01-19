1553272f2613513c4e0efa6c1ece95b5e496c656
KittenLord <vanya.braslavets@gmail.com>
2025-01-19 18:47:41 +0200

    monad

--- MyRandom.hs
+++ MyRandom.hs
@@ -20,7 +20,24 @@ randomInt = Random $ \g -> let x = (rngA * g + rngC) `mod` rngM in
 instance Functor Random where
     fmap f (Random t) = Random $ \g -> let (a, g') = t g in (f a, g')
 
+instance Applicative Random where
+    pure a = Random $ \g -> (a, g)
+    (Random fc) <*> (Random ac) = Random $
+        \g -> let (f, g) = fc g in
+              let (a, g) = ac g in
+              (f a, g)
 
+instance Monad Random where
+    (Random ac) >>= f = Random $ (\(a, g') -> let (Random bc) = f a in bc g') . ac
 
 pickRandom :: [a] -> Random a
-pickRandom xs = (\a -> xs !! (a `mod` length xs)) <$> randomInt
+pickRandom xs = do
+    a <- randomInt
+    let i = a `mod` length xs
+    return $ xs !! a
+
+randomPair :: Random (Int, Int)
+randomPair = do
+    x <- randomInt
+    y <- randomInt
+    return (x, y)
